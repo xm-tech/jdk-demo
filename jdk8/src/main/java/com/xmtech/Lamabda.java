6c1b03c514a75ea1243b9bfa87f160235fdc06e1
xm-tech <maxm09@163.com>
2018-09-29 14:26:46 +0800

    add test case

---
+++ jdk8/src/main/java/com/xmtech/Lamabda.java
@@ -0,0 +1,25 @@
+package com.xmtech;
+
+public class Lamabda {
+
+    private void test() {
+        final Runnable task = () -> {
+            out("exec task ");
+        };
+
+        new Thread(task).start();
+    }
+
+
+    public void out(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void main(String[] args) {
+        Lamabda e = new Lamabda();
+        e.test();
+//        e.testStream();
+    }
+
+
+}
